6e58de6f6a1e046b209fbc1a9e7fb46b350f0d5f
david <davidsanchezdiaz72@gmail.com>
2024-01-10 20:28:39 +0100

    Mejora para intentar solucionar error en null task

--- app/Livewire/Task.php
+++ app/Livewire/Task.php
@@ -14,37 +14,48 @@ class Task extends Component
 
     public function mount()
     {
+        var_dump('mount');
         $this->task = new ModelsTask();
         $this->tasks = ModelsTask::get();
     }
 
     public function updatedTask()
     {
-        dd($this->task['text']);
+        var_dump('updatedTask');
+        //dd($this->task['text']);
     }
 
     public function save()
     {
+        var_dump('save');
+        
         $this->validate();
+        var_dump($this->task->text); // Acceder a la propiedad text de la instancia ModelsTask
 
         $errors = $this->getErrorBag();
 
         if ($errors->any()) {
             dd($errors->all());
         }
-    
-        if (!empty($this->task['text'])) {
-            $task = new ModelsTask(['text' => $this->task['text']]);
+
+        if (!empty($this->task->text)) {
+            var_dump('entro en if de save');
+
+            $task = new ModelsTask(['text' => $this->task->text]); // Acceder a la propiedad text de la instancia ModelsTask
             $task->save();
             $this->mount();
             session()->flash('message', 'Tarea guardada correctamente!');
         } else {
+            var_dump('else de save');
+
             session()->flash('error', 'El campo de texto no puede estar vacío.');
         }
     }
 
     public function render()
     {
+        var_dump('render');
+
         return view('livewire.task', [
             'tasks' => $this->tasks,
         ]);
--- resources/views/livewire/task.blade.php
+++ resources/views/livewire/task.blade.php
@@ -1,7 +1,7 @@
 <div>
     <form class="p-4" wire:submit.prevent="save" wire:loading.remove>
         <div class="mb-4">
-            <input wire:model.defer="task.text" class="p-2 bg-gray-200 w-full" type="text" placeholder="Tarea...">
+            <input wire:model="task.text" class="p-2 bg-gray-200 w-full" type="text" placeholder="Tarea...">
             @error('task.text')
                 <div class="mt-1 text-red-600 text-sm">
                     {{ $message }}
@@ -24,7 +24,6 @@
             </tr>
         </thead>
         <tbody class="text-gray-600">
-
             @forelse($tasks as $task)
                 <tr class="border-b border-gray-200">
                     <td class="px-4 py-2"><input type="checkbox"></td>
@@ -35,7 +34,9 @@
                     </td>
                 </tr>
             @empty
-                <h3>No existen tareas para mostrar</h3>
+                <tr>
+                    <td colspan="3" class="text-center py-4">No existen tareas para mostrar</td>
+                </tr>
             @endforelse
         </tbody>
     </table>
